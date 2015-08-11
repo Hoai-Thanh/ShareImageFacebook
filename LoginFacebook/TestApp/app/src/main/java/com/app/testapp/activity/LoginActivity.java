761f7ab187fb62d36d478c003d77e8e18ae6f782
Nguyen Hoai Thanh <hoaithanh.agilsun@gmail.com>
2015-08-11 10:59:22 +0700

    build release apk

--- LoginFacebook/TestApp/app/src/main/java/com/app/testapp/activity/LoginActivity.java
+++ LoginFacebook/TestApp/app/src/main/java/com/app/testapp/activity/LoginActivity.java
@@ -3,12 +3,20 @@ package com.app.testapp.activity;
 import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 
 import com.app.testapp.R;
 import com.app.testapp.fragment.LoginFragment;
 import com.app.testapp.fragment.ShareFragment;
 import com.app.testapp.listener.FragmentChangeListener;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class LoginActivity extends Activity implements FragmentChangeListener {
@@ -35,7 +43,24 @@ public class LoginActivity extends Activity implements FragmentChangeListener {
             transaction.addToBackStack(null);
             transaction.commit();
             from = "login";
+        }
 
+        // key hash
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(),
+                    PackageManager.GET_SIGNATURES);
+
+            for (Signature signature : info.signatures)
+            {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("name not found", e.toString());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("no such an algorithm", e.toString());
         }
     }
 
